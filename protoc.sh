9bccd7b17c7f31522d78730ddfa1e893c10b7d70
Sujh <sujunhao2568@163.com>
2026-06-11 14:08:36 +0800

    order-update

--- protoc.sh
+++ protoc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SERVICE_NAME = $1
+SERVICE_NAME=$1
 RELEASE_VERSION=$2
 USER_NAME=$3
 EMAIL=$4
@@ -15,20 +15,21 @@ sudo apt-get install -y protobuf-compiler golang-goprotobuf-dev
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 
+mkdir -p golang
 protoc \
     --go_out=./golang --go_opt=paths=source_relative \
     --go-grpc_out=./golang --go-grpc_opt=paths=source_relative \
-    ./${SERVICE_NAME}/*.proto
+    ./"${SERVICE_NAME}"/*.proto
 
-cd golang/${SERVICE_NAME}
-go mod init github.com/mooncell-bb/microservices-proto/golang/${SERVICE_NAME} || true
+cd golang/"${SERVICE_NAME}" || exit 1
+go mod init github.com/mooncell-bb/microservices-proto/golang/"${SERVICE_NAME}" || true
 go mod tidy
 
 # Git 提交
-cd ../../
+cd "${GITHUB_WORKSPACE}" || cd ../..
 git add . && git commit -am "proto update" || true
 git push origin HEAD:main
 git tag \
-    -fa golang/${SERVICE_NAME}/${RELEASE_VERSION} \
+    -fa golang/"${SERVICE_NAME}"/"${RELEASE_VERSION}" \
     -m "golang/${SERVICE_NAME}/${RELEASE_VERSION}"
-git push origin refs/tags/golang/${SERVICE_NAME}/${RELEASE_VERSION}
+git push origin refs/tags/golang/"${SERVICE_NAME}"/"${RELEASE_VERSION}"
